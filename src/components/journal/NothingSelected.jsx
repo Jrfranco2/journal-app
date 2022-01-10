690953e26c569d50597984f57c35a5139f175fd0
Jonathan Franco <jonas150397@gmail.com>
2022-01-10 16:26:36 -0500

    add notes reducer

--- src/components/journal/NothingSelected.jsx
+++ src/components/journal/NothingSelected.jsx
@@ -2,7 +2,7 @@ const NothingSelected = () => {
   return (
     <div className="nothing_main-content">
       <p>
-        Select somethinh
+        Select something
         <br />
         or create an entry!
       </p>
